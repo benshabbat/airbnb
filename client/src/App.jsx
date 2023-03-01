dfd111f16963d0a075e2c5d34537fe9971f36766
benshabbat <benshabbat27@gmail.com>
2023-03-01 19:47:50 +0200

    use react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
-
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Header/>
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
     </div>
   );
 };
