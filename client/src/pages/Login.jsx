ac4bb02ba716dec4903f1008a4178023a6a9c46f
benshabbat <benshabbat27@gmail.com>
2023-03-01 23:51:10 +0200

    ui login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,8 +9,8 @@ const Login = () => {
         <input type="email" placeholder="your@email.com" />
         <input type="password" placeholder="password" />
         <button className="primary">Login</button>
-        <div className="text-center py-2">
-          Don't have an account yet? <Link to="/register">Register Now</Link>
+        <div className="text-center py-2 text-gray-500">
+          Don't have an account yet? <Link className="underline text-blue-600" to="/register">Register Now</Link>
         </div>
       </form>
     </div>
