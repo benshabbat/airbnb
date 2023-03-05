a784a4102b68e5b770e60c52347bd220c2507eb7
benshabbat <benshabbat27@gmail.com>
2023-03-05 23:55:02 +0200

    add jwt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,7 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 // import { createError } from "../utils/error.js";
-// import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
   const { username, email, password } = req.body;
