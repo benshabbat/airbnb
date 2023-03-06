031d2fd865aa041524b6056915ad01d1e2eb7f8b
benshabbat <benshabbat27@gmail.com>
2023-03-06 14:21:39 +0200

    add cookie parser

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js"
 import authRoute from "./routes/auth.js";
 const app= express();
 dotenv.config();
 
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
     credentials :true,
