6a387c2b35f9f25783eec3de64cf8a09b6aec664
benshabbat <benshabbat27@gmail.com>
2023-03-08 01:46:03 +0200

    i dont need usercontext rn

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,11 @@ import App from "./App";
 import "./index.css";
 
 import { AuthContextProvider } from "./context/AuthContext";
-import { UserContextProvider } from "./context/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <UserContextProvider>
-        <App />
-      </UserContextProvider>
+      <App />
     </AuthContextProvider>
   </React.StrictMode>
 );
