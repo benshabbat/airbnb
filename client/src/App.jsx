35a7d92e2f74f7d069f95e187e668308bde6da81
benshabbat <benshabbat27@gmail.com>
2023-03-09 19:16:35 +0200

    account ui with some comps with routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Account from "./pages/Account";
+import Booking from "./components/Booking";
+import Places from "./components/Places";
+
 import axios from "axios";
 axios.defaults.baseURL = "http://127.0.0.1:8080/api";
 axios.defaults.withCredentials = true;
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -14,7 +18,11 @@ const App = () => {
         <Route path="/" element={<Header />}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/account" element={<Account />} />
+          <Route path="/account" element={<Account />}>
+            {/* <Route path=":sub" element={<Account />} /> */}
+            <Route path="bookings" element={<Booking />} />
+            <Route path="places" element={<Places />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { Link, Navigate } from "react-router-dom";
+import { Link, Navigate,Outlet } from "react-router-dom";
 
 const Account = () => {
   const { user } = useContext(AuthContext);
@@ -10,11 +10,12 @@ const Account = () => {
   }
   return (
     <div>
-      <nav className="w-full flex mt-4">
-        <Link className="p-2 bg-gray-400 rounded-full" to={"/account/"}>My Profile</Link>
-        <Link className="p-2" to={"/account/bookings"}>My Bookings</Link>
-        <Link className="p-2" to={"/account/places"}>My Accommodations</Link>
+      <nav className="w-full flex justify-center mt-8 gap-2">
+        <Link className="py-2 px-6 bg-red-700 text-white rounded-full" to={"/account"}>My Profile</Link>
+        <Link className="py-2 px-6" to={"bookings"}>My Bookings</Link>
+        <Link className="py-2 px-6" to={"places"}>My Accommodations</Link>
       </nav>
+      <Outlet />
     </div>
   );
 };
