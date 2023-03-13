efe43c8a2f92b798b41a0e73ec9f0ddd58ee20dc
benshabbat <benshabbat27@gmail.com>
2023-03-13 22:12:28 +0200

    some ui

--- client/src/pages/Places.jsx
+++ client/src/pages/Places.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Places = () => {
   return (
-    <div>Places</div>
-  )
-}
+    <div>
+      <div className="text-center ">
+        <Link
+          className="inline-flex gap-1 bg-red-700 text-white rounded-full py-2 px-6"
+          to={"/account/places/new"}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+          Add new place
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Places
+export default Places;
