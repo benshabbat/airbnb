61ff05b574ebeab37379b88edc1a6b87dad3dba2
benshabbat <benshabbat27@gmail.com>
2023-03-26 21:57:44 +0300

    create nav account

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from "../context/AuthContext";
 import { Link, Navigate, useParams } from "react-router-dom";
 import { AiOutlineUnorderedList, AiOutlineUser } from "react-icons/ai";
 import { HiOutlineHomeModern } from "react-icons/hi2";
+import NavAccount from "../components/NavAccount";
 import Places from "./Places";
 import Profile from "./Profile";
 const Account = () => {
@@ -16,31 +17,9 @@ const Account = () => {
   if (sub === undefined) {
     sub = "profile";
   }
-  const linkClasses = (type) => {
-    let classes = "inline-flex gap-1 py-2 px-6 rounded-full ";
-    if (type === sub) {
-      classes += " bg-red-700 text-white";
-    } else {
-      classes += " bg-gray-200";
-    }
-    return classes;
-  };
   return (
     <div>
-      <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
-        <Link className={linkClasses("profile")} to={"/account"}>
-          <AiOutlineUser className="h-6 w-6" />
-          My Profile
-        </Link>
-        <Link className={linkClasses("bookings")} to={"/account/bookings"}>
-          <AiOutlineUnorderedList className="h-6 w-6" />
-          My Bookings
-        </Link>
-        <Link className={linkClasses("places")} to={"/account/places"}>
-          <HiOutlineHomeModern className="h-6 w-6" />
-          My Accommodations
-        </Link>
-      </nav>
+      <NavAccount />
       {sub === "profile" && <Profile user={user} />}
       {sub === "places" && <Places user={user} />}
     </div>
