63b223fb1293b175315387dc0e714c19a048689f
benshabbat <benshabbat27@gmail.com>
2023-03-26 23:02:38 +0300

    get place and get places in routes

--- api/routes/places.js
+++ api/routes/places.js
@@ -1,13 +1,17 @@
 import express from "express";
-import { uploadByLink,upload,createPlace} from "../controllers/place.js";
+import { uploadByLink,upload,createPlace, getPlace, getPlaces} from "../controllers/place.js";
 import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 import multer from "multer"
 const router = express.Router();
 const photosMiddleware = multer({dest:'/uploads'})
 
-//CREATE
 router.post("/upload",photosMiddleware.array("photos",100), verifyToken, upload);
 router.post("/upload-link", verifyToken, uploadByLink);
+//CREATE
 router.post("/:userId",verifyToken,createPlace);
+//GET
+router.get("/:id",verifyToken, getPlace);
+//GET ALL
+router.get("/",verifyToken, getPlaces);
 // router.get("/profile",profile);
 export default router;
