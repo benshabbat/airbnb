e41eca057295750cad4572377006ff878659d397
benshabbat <benshabbat27@gmail.com>
2023-03-26 22:57:29 +0300

    prittier nav account

--- client/src/components/NavAccount.jsx
+++ client/src/components/NavAccount.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
+import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { AiOutlineUnorderedList, AiOutlineUser } from "react-icons/ai";
 import { HiOutlineHomeModern } from "react-icons/hi2";
 const NavAccount = () => {
-    let { sub } = useParams();
-    if (sub === undefined) {
-      sub = "profile";
+  let { sub } = useParams();
+  if (sub === undefined) {
+    sub = "profile";
+  }
+  const linkClasses = (type) => {
+    let classes = "inline-flex gap-1 py-2 px-6 rounded-full ";
+    if (type === sub) {
+      classes += " bg-red-700 text-white";
+    } else {
+      classes += " bg-gray-200";
     }
-    const linkClasses = (type) => {
-      let classes = "inline-flex gap-1 py-2 px-6 rounded-full ";
-      if (type === sub) {
-        classes += " bg-red-700 text-white";
-      } else {
-        classes += " bg-gray-200";
-      }
-      return classes;
-    };
+    return classes;
+  };
   return (
     <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
-    <Link className={linkClasses("profile")} to={"/account"}>
-      <AiOutlineUser className="h-6 w-6" />
-      My Profile
-    </Link>
-    <Link className={linkClasses("bookings")} to={"/account/bookings"}>
-      <AiOutlineUnorderedList className="h-6 w-6" />
-      My Bookings
-    </Link>
-    <Link className={linkClasses("places")} to={"/account/places"}>
-      <HiOutlineHomeModern className="h-6 w-6" />
-      My Accommodations
-    </Link>
-  </nav>
-  )
-}
+      <Link className={linkClasses("profile")} to={"/account"}>
+        <AiOutlineUser className="h-6 w-6" />
+        My Profile
+      </Link>
+      <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+        <AiOutlineUnorderedList className="h-6 w-6" />
+        My Bookings
+      </Link>
+      <Link className={linkClasses("places")} to={"/account/places"}>
+        <HiOutlineHomeModern className="h-6 w-6" />
+        My Accommodations
+      </Link>
+    </nav>
+  );
+};
 
-export default NavAccount
+export default NavAccount;
