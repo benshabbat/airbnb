0da91b2acef37b7f112844db6267523822678dc5
benshabbat <benshabbat27@gmail.com>
2023-04-02 00:22:54 +0300

    select photo to main photo for space

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { FaTrashAlt } from "react-icons/fa";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
-
-const Photo = ({photos,setPhotos}) => {
-  const removePhoto =(link)=>{
-    
-    setPhotos([...photos.filter(selectedName=>selectedName!==link)]);
-  }
+const Photo = ({ photos, setPhotos }) => {
+  const removePhoto = (e, link) => {
+    e.preventDefault();
+    setPhotos([...photos.filter((selectedName) => selectedName !== link)]);
+  };
+  const selectMainPhoto = (e, link) => {
+    e.preventDefault();
+    setPhotos([
+      link,
+      ...photos.filter((selectedName) => selectedName !== link),
+    ]);
+  };
   return photos.map((link, index) => {
     return (
       <div key={index} className="flex h-32 relative">
@@ -14,8 +21,17 @@ const Photo = ({photos,setPhotos}) => {
           className="rounded-2xl w-full object-cover"
           src={"http://localhost:8080/api/uploads/" + link}
         />
-        <button onClick={()=>removePhoto(link)} className="absolute bottom-1 right-1 text-white bg-black bg-opacity-50 rounded-2xl p-1 cursor-pointer">
+        <button
+          onClick={(e) => removePhoto(e, link)}
+          className="absolute bottom-1 right-1 text-white bg-black bg-opacity-50 rounded-2xl p-1 cursor-pointer"
+        >
           <FaTrashAlt />
+        </button>
+        <button
+          onClick={(e) => selectMainPhoto(e, link)}
+          className="absolute bottom-1 left-1 text-white bg-black bg-opacity-50 rounded-2xl p-1 cursor-pointer"
+        >
+          {link === photos[0] ? <AiFillStar /> : <AiOutlineStar />}
         </button>
       </div>
     );
