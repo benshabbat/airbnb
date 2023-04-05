f4fd43424dc13ebd60c756c8e300cfa1596c886e
benshabbat <benshabbat27@gmail.com>
2023-04-05 16:07:06 +0300

    created bookig then form

--- client/src/components/Booking.jsx
+++ client/src/components/Booking.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const Booking = ({place}) => {
+  const [formData, setFormData] = useState({
+    checkIn: "",
+    checkOut: "",
+    maxGuests: 1,
+    price: place?.price,
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
   return (
     <div className="bg-gray-100 shadow p-4 rounded-2xl my-4">
           <div className="text-center text-xl">
@@ -10,19 +23,19 @@ const Booking = ({place}) => {
             <div className="flex">
               <div className="py-3 px-4">
                 <label>Check In:</label>
-                <input type="date" />
+                <input type="date" name={checkIn} onChange={handleChange} />
               </div>
               <div className="py-3 px-4 border-l">
                 <label>Check Out:</label>
-                <input type="date" />
+                <input type="date" name={checkOut} onChange={handleChange} />
               </div>
             </div>
             <div className="py-3 px-4 border-t">
               <label>Number Of Guests:</label>
-              <input type="number" />
+              <input type="number" name={maxGuests} onChange={handleChange} />
             </div>
           </div>
-          <button className="primary">Booking</button>
+          <button className="primary" type="submit">Booking</button>
         </div>
   )
 }
