990c49bb3d90b9847eb59a99a5f7b9541e0379b2
benshabbat <benshabbat27@gmail.com>
2023-04-06 22:35:27 +0300

    booking inputdata

--- client/src/components/Booking.jsx
+++ client/src/components/Booking.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const Booking = ({place}) => {
+const Booking = ({ place }) => {
   const [formData, setFormData] = useState({
     checkIn: "",
     checkOut: "",
@@ -14,30 +14,46 @@ const Booking = ({place}) => {
       [name]: value,
     }));
   };
+  const inputData = (type, name) => {
+    return (
+      <input
+        type={type}
+        placeholder={name}
+        value={formData[name]}
+        name={name}
+        onChange={handleChange}
+        // required
+      />
+    );
+  };
   return (
     <div className="bg-gray-100 shadow p-4 rounded-2xl my-4">
-          <div className="text-center text-xl">
-            Price:${place?.price} / per night
-          </div>
-          <div className="border rounded-2xl mt-4">
-            <div className="flex">
-              <div className="py-3 px-4">
-                <label>Check In:</label>
-                <input type="date" name={checkIn} onChange={handleChange} />
-              </div>
-              <div className="py-3 px-4 border-l">
-                <label>Check Out:</label>
-                <input type="date" name={checkOut} onChange={handleChange} />
-              </div>
-            </div>
-            <div className="py-3 px-4 border-t">
-              <label>Number Of Guests:</label>
-              <input type="number" name={maxGuests} onChange={handleChange} />
-            </div>
-          </div>
-          <button className="primary" type="submit">Booking</button>
+      <form onSubmit={onSubmit}>
+        <div className="text-center text-xl">
+          Price:${place?.price} / per night
         </div>
-  )
-}
+        <div className="border rounded-2xl mt-4">
+          <div className="flex">
+            <div className="py-3 px-4">
+              <label>Check In:</label>
+              {inputData("date", "checkIn")}
+            </div>
+            <div className="py-3 px-4 border-l">
+              <label>Check Out:</label>
+              {inputData("date", "checkOut")}
+            </div>
+          </div>
+          <div className="py-3 px-4 border-t">
+            <label>Number Of Guests:</label>
+            {inputData("number", "maxGuests")}
+          </div>
+          <button className="primary" type="submit">
+            Booking
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Booking
+export default Booking;
