830a08ca8cbab11892ba98eb3021d3a91ef83aab
benshabbat <benshabbat27@gmail.com>
2023-04-09 01:10:41 +0300

    place img to place

--- client/src/components/Place.jsx
+++ client/src/components/Place.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import AddPlace from "./AddPlace";
-import Image from "./Image";
+import PlaceImg from "./PlaceImg";
 const Place = ({ place }) => {
   const { action } = useParams();
   return (
@@ -13,14 +13,10 @@ const Place = ({ place }) => {
           className="flex flex-col gap-4 bg-gray-100 p-4 rounded-2xl"
         >
           <div className="flex w-32 h-32 grow shrink-0">
-            <Image
-              className="rounded-2xl w-full object-cover"
-              src={place.photos?.[0]}
+            <PlaceImg
+              className={"rounded-2xl w-full object-cover"}
+              place={place}
             />
-
-            {/* {place.photos?.map((link, index) => {
-            return <Photo key={index} link={link} />;
-          })} */}
           </div>
           <div className="grow-0 shrink">
             <h2 className="text-xl">{place.title}</h2>
--- client/src/components/PlaceImg.jsx
+++ client/src/components/PlaceImg.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Image from './Image';
 
-const PlaceImg = ({place,className,index}) => {
-    if (!place.photos?.length) {
+const PlaceImg = ({place,className=null,index=0}) => {
+    if (!place?.photos?.length) {
         return '';
       }
       if (!className) {
