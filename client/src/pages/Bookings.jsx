6018753076adc04114b9b40ed6459a6250841b8a
benshabbat <benshabbat27@gmail.com>
2023-04-09 09:29:21 +0300

    format on dates

--- client/src/pages/Bookings.jsx
+++ client/src/pages/Bookings.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getBookings, getBookingsByOwner } from "../Utils";
 import { useParams } from "react-router-dom";
 import PlaceImg from "../components/PlaceImg";
+import { format } from "date-fns";
 const Bookings = ({ user }) => {
   const [bookings, setBookings] = useState([]);
   const { action } = useParams();
@@ -22,11 +23,17 @@ const Bookings = ({ user }) => {
       {bookings?.map?.((booking) => {
         return (
           <div key={booking._id}>
-            <div>
+            <div className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden">
               <div className="w-48">
-                <PlaceImg place={booking.place} />
+                <PlaceImg
+                  place={booking.place}
+                  className={"rounded-2xl object-cover aspect-square"}
+                />
               </div>
-              {booking.checkIn} {booking.checkOut}
+              <div className="py-4">
+              <h2 className="text-xl">{booking.place.title}</h2>
+                {format(new Date(booking?.checkIn),"yyyy-MM-dd") } {format(new Date(booking?.checkOut),"yyyy-MM-dd")}
+              </div>
             </div>
           </div>
         );
